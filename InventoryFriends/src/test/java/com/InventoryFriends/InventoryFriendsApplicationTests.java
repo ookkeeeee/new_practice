decce42138fa6f3e2cbc29e260ca68bb2ea3176c
DESKTOP-JI2T3EG\sjshi <sjs818@theloca.co.kr>
2023-01-25 16:01:11 +0900

    프로젝트 생성

---
+++ InventoryFriends/src/test/java/com/InventoryFriends/InventoryFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InventoryFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Inventory Friends
+
+## 2023.01.25 프로젝트 시작
